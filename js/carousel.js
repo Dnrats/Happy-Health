c793532cbcf4d2eb3dac9ff19618d29ef35fe268
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-05-31 11:23:46 +0200

    del:logo

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+function ouioui() {
+  const next = document.querySelector(".fa-arrow-right");
+  const prev = document.querySelector(".fa-arrow-left-long");
+
+  const allCarousel = document.querySelectorAll(".slider");
+  const all = allCarousel.length;
+
+  
+}
+
+ouioui();
