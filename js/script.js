d82fbe3bdd777917b5f93a4e226582ef9989f421
Artem <artemfr@hotmail.com>
2022-06-01 13:57:28 +0200

    fixed the problem with the chart

--- js/script.js
+++ js/script.js
@@ -1,12 +1,53 @@
-let myChart = document.getElementById('myChart');
-const labels = Utils.months({count: 7});
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
-};
+
+// let labels1 =  ['Yes', 'No'];
+// let data1 = [55, 36];
+// let colors1 = ['black', 'yellow'];
+
+
+// let myChart = document.getElementById('myChart').getContext('2d');
+// let chaart = new Chart(myChart, {
+//     type: 'doughnut',
+//     data: {
+//         labels: labels1,
+//         datasets: [ {
+//             data: data1,
+//             backgroundColor: colors1
+//         }]
+//     },
+//     options: {
+//         title: {
+//             text: "Duh, its a donut!",
+//             display: true
+//         }
+//     }
+// });
+
+
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
