0d51901e92380b070762e37bcce04839499c68ae
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-06-01 16:22:03 +0200

    ajout resposive mobile + pc

--- js/carousel.js
+++
@@ -1,5 +0,0 @@
-let currentSlide = 0;
-const next = document.querySelector(".fa-arrow-right");
-const prev = document.querySelector(".fa-arrow-left-long");
-const slides = document.querySelectorAll(".slide");
-let maxSlide = slides.length - 1;
