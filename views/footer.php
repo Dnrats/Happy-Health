496acfe48eb43fee9978f9d5aaa062ecb539019e
Ogu <august.gros@gmail.com>
2022-06-07 14:05:07 +0200

    modif page html en php

---
+++ views/footer.php
@@ -0,0 +1,12 @@
+    <footer>
+      <hr />
+      <p>
+        &copy; 2022 Floriane Simmet - Made with
+        <i class="fa-solid fa-heart"></i>
+      </p>
+    </footer>
+    <script src="./js/citation.js"></script>
+    <script src="./js/graph.js"></script>
+    <script src="./controllers/home_ajax.js"></script>
+  </body>
+</html>
