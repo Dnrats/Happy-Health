c7b4133bed389e37a554f7a164d74bd12ac40823
Ogu <august.gros@gmail.com>
2022-06-08 12:08:20 +0200

    add script graph

--- models/graph_home.php
+++ models/graph_home.php
@@ -3,11 +3,11 @@
 /* Connexion pdo */
 include "./db_login.php";
 
-$stmt = $db->prepare('SELECT ROUND(AVG(val), 2), `year` FROM country_has_years
+$stmt = $db->prepare('SELECT ROUND(AVG(val), 2) AS valAVG, `year` FROM country_has_years
                         INNER JOIN vals ON happiness_score = id_val
                         INNER JOIN years ON years_id = id_years
                         GROUP BY `year`');
 $stmt->execute();
 $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-json_encode($res);
+echo json_encode($res);
