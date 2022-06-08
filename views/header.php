c3eee2bf46ee3c28b99417de5ac44ce496ac6422
Ogu <august.gros@gmail.com>
2022-06-08 12:26:49 +0200

    add graph home + fix link

--- views/header.php
+++ views/header.php
@@ -47,11 +47,11 @@
     <header>
       <div class="nav-container">
         <!-- Logo & title -->
-        <img src="./assets/logo.svg" alt="" />
+        <a href=""><img src="./assets/logo.svg" alt="logo du site" /></a>
         <nav>
           <ul>
-            <li><a class="happy" href="#">Happy</a></li>
-            <li><a class="health" href="#">Health</a></li>
+            <li><a class="happy" href="happiness">Happy</a></li>
+            <li><a class="health" href="health">Health</a></li>
           </ul>
         </nav>
       </div>
